ae9e66fb01ffadd0b945f1dea2bd5bcde1bf6246
Gábor Németh <gabor.d.nemeth@gmail.com>
2023-10-08 22:27:12 +0200

    Outsourced PersonListItem.tsx

---
+++ react_app/src/views/PersonsView/PersonListItem.tsx
@@ -0,0 +1,60 @@
+import DeleteIcon from '@mui/icons-material/Delete'
+import {
+  Avatar,
+  IconButton,
+  ListItem,
+  ListItemAvatar,
+  ListItemButton,
+  ListItemText
+} from '@mui/material'
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import { useDeletePersons } from '../../api/hooks/person'
+import { Person } from '../../types'
+
+import { getPersonsName } from './utils/getPersonsName'
+
+export const PersonListItem = ({
+  person: { id, name, games, url }
+}: {
+  person: Person
+}) => {
+  const { mutateAsync } = useDeletePersons(id)
+  const navigate = useNavigate()
+
+  const handleDelete = async () => {
+    await mutateAsync()
+  }
+
+  const handleSelectPerson = () => {
+    navigate(`/persons/${id}`)
+  }
+
+  return (
+    <ListItem
+      key={id}
+      disablePadding
+      secondaryAction={
+        <IconButton
+          edge="end"
+          aria-label="delete"
+          color={'error'}
+          onClick={handleDelete}
+        >
+          <DeleteIcon />
+        </IconButton>
+      }
+    >
+      <ListItemButton onClick={handleSelectPerson}>
+        <ListItemAvatar>
+          <Avatar src={url} />
+        </ListItemAvatar>
+        <ListItemText
+          primary={getPersonsName(name)}
+          secondary={`${games.length} games`}
+        />
+      </ListItemButton>
+    </ListItem>
+  )
+}
--- react_app/src/views/PersonsView/PersonsList.tsx
+++ react_app/src/views/PersonsView/PersonsList.tsx
@@ -1,59 +1,21 @@
 import AddIcon from '@mui/icons-material/Add'
-import DeleteIcon from '@mui/icons-material/Delete'
-import {
-  Avatar,
-  IconButton,
-  ListItem,
-  ListItemAvatar,
-  ListItemButton,
-  ListItemText
-} from '@mui/material'
+import { IconButton, ListItem } from '@mui/material'
 import List from '@mui/material/List'
 import React from 'react'
 
-import { useDeletePersons } from '../../api/hooks/person'
 import { Person } from '../../types'
 
-import { getPersonsName } from './utils/getPersonsName'
+import { PersonListItem } from './PersonListItem'
 
 interface Props {
   persons: Person[]
-  onPersonSelected: (person: Person) => void
 }
 
-export const PersonsList: React.FC<Props> = ({ persons, onPersonSelected }) => {
-  const { mutateAsync } = useDeletePersons()
-  const handleDelete = async (id: Person['id']) => {
-    await mutateAsync(id)
-  }
-
+export const PersonsList: React.FC<Props> = ({ persons }) => {
   return (
     <List sx={{ width: 250 }}>
       {persons.map((person) => (
-        <ListItem
-          key={person.id}
-          disablePadding
-          secondaryAction={
-            <IconButton
-              edge="end"
-              aria-label="delete"
-              color={'error'}
-              onClick={() => handleDelete(person.id)}
-            >
-              <DeleteIcon />
-            </IconButton>
-          }
-        >
-          <ListItemButton onClick={() => onPersonSelected(person)}>
-            <ListItemAvatar>
-              <Avatar src={person.url} />
-            </ListItemAvatar>
-            <ListItemText
-              primary={getPersonsName(person.name)}
-              secondary={`${person.games.length} games`}
-            />
-          </ListItemButton>
-        </ListItem>
+        <PersonListItem person={person} />
       ))}
       <ListItem sx={{ justifyContent: 'center' }}>
         <IconButton>
